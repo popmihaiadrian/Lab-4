5372c3323590a84c597af2bff88e167ca7081dc2
Mihai Adrian Pop <popmihaiadrian@gmail.com>
2019-06-22 10:28:19 +0200

    Final commit Lab 4

---
+++ Lab-2-webapi/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab_2_webapi.Models;
+using Lab_2_webapi.Services;
+using Lab_2_webapi.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_2_webapi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUsersService _userService;
+
+        public UsersController(IUsersService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]LoginPostModel login)
+        {
+            var user = _userService.Authenticate(login.Username, login.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody]RegisterPostModel registerModel)
+        {
+            var user = _userService.Register(registerModel);
+            if (user == null)
+            {
+                return BadRequest(new { ErrorMessage = "Username already exists." });
+            }
+            return Ok(user);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ Lab-2-webapi/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_2_webapi.Models
+{
+    public enum UserRole
+    {
+        Regular,
+        UserManager,
+        Admin,
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [EnumDataType(typeof(UserRole))]
+        public UserRole UserRole { get; set; }
+    }
+}
